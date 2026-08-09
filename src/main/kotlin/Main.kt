77734fbe0151a02279d71db845540c7d77adc203
Yitz <yitzlubin@gmail.com>
2026-08-09 18:29:04 -0400

    Initialize Ktor server with basic routes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello from Kotlin on SnapDeploy!")
+            }
+            get("/ping") {
+                call.respondText("pong")
+            }
+        }
+    }.start(wait = true)
+}
